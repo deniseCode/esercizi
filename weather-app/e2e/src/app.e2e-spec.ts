24cbf49ed0b8385fb34a6424996b1ed255266e6f
Denise Anchisi <denise.anchisi@avanade.com>
2021-05-05 16:23:57 +0200

    Delete all and create new project

--- weather-app/e2e/src/app.e2e-spec.ts
+++ weather-app/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('app-meteo app is running!');
+    expect(await page.getTitleText()).toEqual('weather-app app is running!');
   });
 
   afterEach(async () => {
